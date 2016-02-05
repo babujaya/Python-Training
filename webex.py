f691342f51f9cdf441b90dacbf45a5d1f7feca5a
Jayaraman Babu <jbabu86@yahoo.co.in>
2016-02-04 16:29:27 -0800

    Hello Messenger

--- webex.py
+++ webex.py
@@ -1,1 +1,17 @@
-add team
+#!/usr/local/bin/python3
+
+import subprocess
+
+crazy = 1
+sane = 2
+what_to_print = ''
+
+if crazy == sane:
+   what_to_print = 'hello world'
+else:
+	print ('thats crazy')
+
+if crazy == sane:
+	print ('thats not crazy')
+else:
+	print ('thats crazy')
